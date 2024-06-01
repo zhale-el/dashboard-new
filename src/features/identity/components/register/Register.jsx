8a90a84cfc8313148f5ce4e520c5d96797ff33e9
zhaleh <zhalehelmi@gmail.com>
2024-06-01 16:19:53 +0330

    register hook form

--- src/features/identity/components/register/Register.jsx
+++ src/features/identity/components/register/Register.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import logo from "@assets/images/logo.svg";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 const Register = () => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="text-center">
@@ -21,25 +33,79 @@ const Register = () => {
       <div className="card">
         <div className="card-body">
           <div className="m-sm-4">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-3">
                 <label className="form-label">موبایل</label>
-                <input className="form-control form-control-lg" />
+                <input
+                  {...register("mobile", {
+                    required: "موبایل الرامی است",
+                    minLength: 11,
+                    maxLength: 11,
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.mobile && "is-invalid"
+                  } `}
+                />
+
+                {errors.mobile && errors.mobile.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.mobile?.message}
+                  </p>
+                )}
+                {errors.mobile &&
+                  (errors.mobile.type === "minLength" ||
+                    errors.mobile.type === "maxLength") && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      موبایل باید11 رقم باشد
+                    </p>
+                  )}
               </div>
               <div className="mb-3">
                 <label className="form-label">رمز عبور</label>
                 <input
-                  className="form-control form-control-lg mb-2"
                   type="password"
+                  {...register("password", {
+                    required: "رمز عبور الزامی است",
+                  })}
+                  className={`form-control form-control-lg mb-2 ${
+                    errors.password && "is-invalid"
+                  }`}
                 />
+                {errors.password && errors.password.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.password?.message}
+                  </p>
+                )}
               </div>
 
               <div className="mb-3">
                 <label className="form-label">تکرار رمز عبور</label>
                 <input
-                  className="form-control form-control-lg mb-2"
                   type="password"
+                  {...register("confirmPassword", {
+                    required: "تکرار رمز عبور الزامی است",
+                    validate: (value) => {
+                      if (watch("password") !== value) {
+                        return "عدم تطابق با رمز وارد شده";
+                      }
+                    },
+                  })}
+                  className={`form-control form-control-lg mb-2 ${
+                    errors.confirmPassword && "is-invalid"
+                  }`}
                 />
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "required" && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
+                {errors.confirmPassword &&
+                  errors.confirmPassword.type === "validate" && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
               </div>
 
               <div className="text-center mt-3">
